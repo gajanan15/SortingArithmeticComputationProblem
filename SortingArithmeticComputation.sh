2f9631adbf101f0dbce4cd8e2f829547c675b89b
gajanan15 <gajanandesai97@gmail.com>
2020-01-24 12:09:55 +0530

    ADDED:Fourth Computation

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -7,3 +7,4 @@ read -p "Enter Value Of C: " c
 result1=`echo "scale=2; $a+$b*$c" | bc`
 result2=`echo "scale=2; $a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
+result4=`echo "scale=2; $(($a%$b))+$c" | bc`
