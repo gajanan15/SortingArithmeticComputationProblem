9daa54711d45c9c22c821415e2ddcd4b2c054964
gajanan15 <gajanandesai97@gmail.com>
2020-01-24 12:38:21 +0530

    REFACTORED:corrected the calculations

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -4,7 +4,7 @@ read -p "Enter Value Of A: " a
 read -p "Enter Value Of B: " b
 read -p "Enter Value Of C: " c
 
-result1=`echo "scale=2; $a+$b*$c" | bc`
-result2=`echo "scale=2; $a*$b+$c" | bc`
+result1=`echo "$a+$b*$c" | bc`
+result2=`echo "$a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
-result4=`echo "scale=2; $(($a%$b))+$c" | bc`
+result4=`echo "$a%$b+$c" | bc`
