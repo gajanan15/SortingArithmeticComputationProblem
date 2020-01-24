523d4fade4137aa12132410deb7d4b978e4a2af7
gajanan15 <gajanandesai97@gmail.com>
2020-01-24 14:48:28 +0530

    ADDED:Created array to read dictionary values

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -5,7 +5,7 @@ read -p "Enter Value Of B: " b
 read -p "Enter Value Of C: " c
 
 declare -A ResultDictionary
-
+count=0
 result1=`echo "$a+$b*$c" | bc`
 result2=`echo "$a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
@@ -15,4 +15,11 @@ ResultDictionary[result1]=$result1
 ResultDictionary[result2]=$result2
 ResultDictionary[result3]=$result3
 ResultDictionary[result4]=$result4
+
+while [[ $count -ne ${#ResultDictionary[@]} ]]
+do
+   array[$count]=${ResultDictionary[result"$(( count+1 ))"]}
+   count=$(( count+1 ))
+done
+echo ${array[@]}
 
